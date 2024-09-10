9c951d91eddb05f70315107bcca052eb4e54c0b8
Ferdaus0001 <ferdaushossan7788990@gmail.com>
2024-09-10 15:13:04 +0600

    smart shope

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/cupertino.dart';
+import 'package:get/get.dart';
+import 'package:smartshopes/HomeScreen/HomeScreen.dart';
+
+void main(){
+  runApp(MyApp());
+}
+
+
+
+class MyApp extends StatelessWidget{
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+     return GetMaterialApp(
+       home: HomeScreen(),
+       debugShowCheckedModeBanner: false,
+
+     );
+
+  }
+
+}
